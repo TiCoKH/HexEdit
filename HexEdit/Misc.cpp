4eaab66d907a46cfbfd9e93cbeedf7718f7ee70c
tikviczk <Jozsef.Tikviczki@nokia.com>
2018-12-20 14:32:43 +0100

    update to VS2015

--- HexEdit/Misc.cpp
+++ HexEdit/Misc.cpp
@@ -568,46 +568,12 @@ bool ConvertToFileTime(time_t tt, FILETIME *ft)
 	return retval;
 }
 
-// This is the ptr to (start of) time info from the thread local info structure
-typedef struct {
-        char *wday_abbr[7];
-        char *wday[7];
-        char *month_abbr[12];
-        char *month[12];
-        char *ampm[2];
-        char *ww_sdatefmt;
-        char *ww_ldatefmt;
-        char *ww_timefmt;
-		// ... other fields follow that we are not interested in
-} * plocal_time_info_t;
-
 // Creates a date string using the Windows short date format (eg dd/mm/yy) with 
 // the added feature that 2 digit years are avoided (printed as 4 digits).
 CString FormatDate(DATE dd)
 {
 	// First get the short date format from Windows Control Panel Region Settings
-	CString fmt;
-	UDATE ud;                              // unpacked date
-	_locale_t ploc = _get_current_locale();
-	plocal_time_info_t plti = (plocal_time_info_t)ploc->locinfo->lc_time_curr;
-	fmt = CString(plti->ww_sdatefmt);
-	char buf[80];
-
-	// If format does not have 4 digit year (yyyy) convert 2 digit year to 4 digit year
-	if (fmt.Find("yyyy") == -1 &&
-		fmt.Replace("yy", "yyyy") > 0)
-	{
-		if (::VarUdateFromDate(dd, 0, &ud) == S_OK && 
-			::GetDateFormat(LOCALE_USER_DEFAULT, 0, &ud.st, fmt, buf, sizeof(buf)) > 0)
-		{
-			return CString(buf);
-		}
-		else
-		{
-			return CString("Invalid Date");
-		}
-	}
-
+	// __crt_locale_data not supported in VS2015, keep a fallback code
 	// Fall back on MS code
 	COleDateTime odt;
 	odt.m_dt = dd;
